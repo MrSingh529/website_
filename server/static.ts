b80fa1de9448af1d0b66d71b03593663727f93ed
MrSingh529 <harpinder.singh@rvsolutions.in>
2026-01-01 22:00:38 +0530

    fixed server/static

--- server/static.ts
+++ server/static.ts
@@ -1,19 +1,23 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// ES module equivalent of __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  const distPath = path.resolve(__dirname, "../../dist/public");
+
   if (!fs.existsSync(distPath)) {
-    throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
-    );
+    throw new Error(`Static files not found at ${distPath}`);
   }
 
   app.use(express.static(distPath));
-
-  // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
+  
+  // Serve index.html for all other routes (SPA support)
+  app.get("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
