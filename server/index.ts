72dd7e5c76a511441111e5e6db07f8d29ac1f5b5
MrSingh529 <harpinder.singh@rvsolutions.in>
2026-01-01 21:45:39 +0530

    fixed vite.config.ts final

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
-import { serveStatic } from "./static";
+import { registerRoutes } from "./routes.js";
+import { serveStatic } from "./static.js";
 import { createServer } from "http";
 
 const app = express();
@@ -77,7 +77,7 @@ app.use((req, res, next) => {
   if (process.env.NODE_ENV === "production") {
     serveStatic(app);
   } else {
-    const { setupVite } = await import("./vite");
+    const { setupVite } = await import("./vite.js");
     await setupVite(httpServer, app);
   }
 
@@ -87,9 +87,9 @@ app.use((req, res, next) => {
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "10000", 10);
   httpServer.listen(
-  port,
-  () => {
-    log(`serving on port ${port}`);
-  },
-);
+    port,
+    () => {
+      log(`serving on port ${port}`);
+    },
+  );
 })();
