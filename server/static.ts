0c3db0709532b9530fec1ae2013b3ce14004f9f0
MrSingh529 <harpinder.singh@rvsolutions.in>
2026-02-02 12:07:46 +0530

    Fix: update wildcard route for Express 5.x compatibility

--- server/static.ts
+++ server/static.ts
@@ -23,7 +23,7 @@ export function serveStatic(app: Express) {
   }
 
   app.use(express.static(distPath));
-  app.get("*", (_req, res) => {
+  app.get("/*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
