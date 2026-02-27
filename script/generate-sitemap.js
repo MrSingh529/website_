39e09a3ee5c0f25a6a671c4dc3bb1ab1764dbd20
Harpinder Singh <harpindersingh529@gmail.com>
2026-02-27 14:52:24 +0530

    Update sitemap generation to use new domain

--- script/generate-sitemap.js
+++ script/generate-sitemap.js
@@ -16,7 +16,7 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${pages.map(page => `
     <url>
-      <loc>https://automataxpro.site${page}</loc>
+      <loc>https://automatax.site${page}</loc>
       <changefreq>weekly</changefreq>
       <priority>${page === '/' ? '1.0' : '0.8'}</priority>
     </url>
